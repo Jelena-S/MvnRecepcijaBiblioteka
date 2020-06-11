997995a221d399ce0b5ff32acb18686853d38447
Jelena-S <jsreckovic1@gmail.com>
2020-06-11 12:31:11 +0200

    Dodata dokumentacija za klase StavkaKataloga i UslugaNajma

--- MvnRecepcijaBiblioteka/src/main/java/rs/ac/bg/fon/ai/nprog/MvnRecepcijaBiblioteka/domen/StavkaKataloga.java
+++ MvnRecepcijaBiblioteka/src/main/java/rs/ac/bg/fon/ai/nprog/MvnRecepcijaBiblioteka/domen/StavkaKataloga.java
@@ -9,83 +9,191 @@ import rs.ac.bg.fon.ai.nprog.MvnRecepcijaBiblioteka.domen.Katalog;
 import rs.ac.bg.fon.ai.nprog.MvnRecepcijaBiblioteka.domen.UslugaNajma;
 import rs.ac.bg.fon.ai.nprog.MvnRecepcijaBiblioteka.domen.OpstiDomenskiObjekat;
 
+/**
+ * Klasa predstavlja stavku kataloga sa svim svojim svojstvima. Implementira interfejse OpstiDomenskiObjekat i Serializable
+ *
+ * @Jelena Sreckovic
+ */
+
 public class StavkaKataloga implements OpstiDomenskiObjekat, Serializable{
+	/**
+     * Katalog u kom se stavka kanalazi
+     */
 	private Katalog katalog;
+	
+	/**
+	 * Identifikator stavke kataloga
+	 */
     private Long stavkaKatalogaID;
+    
+    /**
+     * Naziv stavke kataloga
+     */
     private String nazivStavkeKataloga;
+    
+    /**
+     * Usluga najma koju predstavlja stavka kataloga
+     */
     private UslugaNajma uslugaNajma;
 
+    /**
+     * Neparametrizovani konstruktor. Inicijalizuje stavku kataloga
+     */
     public StavkaKataloga() {
     	
     }
 
+    /**
+     * Parametrizovani konstruktor. Inicijalizuje stavku kataloga i postavlja vrednosti atributa pozivanjem set metoda.
+     * 
+     * @param katalog katalog koji je potrebno postaviti
+     * @param stavkaKatalogaID identifikator stavke kataoga koji je potrebno postaviti
+     * @param nazivStavkeKataloga naziv stavke kataloga koji je potrebno postaviti
+     */
     public StavkaKataloga(Katalog katalog, Long stavkaKatalogaID, String nazivStavkeKataloga) {
         this.katalog = katalog;
         this.stavkaKatalogaID = stavkaKatalogaID;
         this.nazivStavkeKataloga = nazivStavkeKataloga;
     }
 
+    /**
+     * Parametrizovani konstruktor. Inicijalizuje stavku kataloga i postavlja vrednost identifikatora na unetu vrednost pozivanjem set metode.
+     * 
+     * @param stavkaKatalogaID identifikator koji je potrebno postaviti
+     */
     public StavkaKataloga(Long stavkaKatalogaID) {
         this.stavkaKatalogaID = stavkaKatalogaID;
     }
 	
+    /**
+     * Metoda vraća katalog u kome se stavka nalazi
+     * 
+     * @return katalog kao Katalog
+     */
 	public Katalog getKatalog() {
 		return katalog;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost kataloga u kome se nalazi stavka 
+	 * 
+	 * @param katalog Katalog koji je potrebno postaviti
+	 */
 	public void setKatalog(Katalog katalog) {
 		this.katalog = katalog;
 	}
 
+	/**
+     * Metoda vraća identifikator stavke kataloga
+     * 
+     * @return stavkaKatalogaID kao Long
+     */
 	public Long getStavkaKatalogaID() {
 		return stavkaKatalogaID;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost identifikatora stavke kataloga
+	 * 
+	 * @param stavkaKatalogaID identifikator stavke kataloga koji je potrebno postaviti
+	 */
 	public void setStavkaKatalogaID(Long stavkaKatalogaID) {
 		this.stavkaKatalogaID = stavkaKatalogaID;
 	}
 
+	/**
+     * Metoda vraća naziv stavke kataloga
+     * 
+     * @return nazivStavkeKataloga kao String
+     */
 	public String getNazivStavkeKataloga() {
 		return nazivStavkeKataloga;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost naziva stavke kataloga
+	 * 
+	 * @param nazivStavkeKataloga naziv stavke kataloga koji je potrebno postaviti
+	 */
 	public void setNazivStavkeKataloga(String nazivStavkeKataloga) {
 		this.nazivStavkeKataloga = nazivStavkeKataloga;
 	}
 
+	/**
+     * Metoda vraća uslugu najma koju stavka kataloga predstavlja
+     * 
+     * @return uslugaNajma kao UslugaNajma
+     */
 	public UslugaNajma getUslugaNajma() {
 		return uslugaNajma;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost usluge najma koju stavka kataloga predstavlja
+	 * 
+	 * @param uslugaNajma usluga najma koji je potrebno postaviti
+	 */
 	public void setUslugaNajma(UslugaNajma uslugaNajma) {
 		this.uslugaNajma = uslugaNajma;
 	}
 
+	/**
+   	 * Metoda vraća naziv tabele u bazi u kojoj se nalaze podaci o stavci kataloga
+   	 * 
+   	 * @return String koji predstavlja naziv tabele.
+   	 */
 	@Override
 	public String getImeTabele() {
 		return "stavkaKataloga";
 	}
 
+	/**
+	 * Metoda vraća vrednosti atributa objekta klase StavkaKataloga
+	 * 
+	 * @return String koji predstavlja vrednosti atributa.
+	 */
 	@Override
 	public String getParametre() {
         return String.format("%s, %s, '%s'", katalog.getKatalogID(),stavkaKatalogaID,nazivStavkeKataloga);
 	}
 
+	/**
+	 * Metoda vraća imena atributa objekta klase StavkaKataloga nad kojim se poziva metoda
+	 * 
+	 * @return String koji predstavlja imena atributa, odvojeni su zapetom.
+	 */
 	@Override
 	public String getImenaParametara() {
 		return "katalogID,stavkaKatalogaID,nazivStavkeKataloga";
 	}
 
+	/**
+   	 * Metoda vraća ime primarnog ključa tabele u bazi u kojoj se nalaze podaci o stavci kataloga
+   	 * 
+   	 * @return null koji predstavlja ime primarnog kljuca, jer je primarni ključ složen. 
+   	 */
 	@Override
 	public String getImePrimarnogKljuca() {
 		return null;
 	}
 
+	/**
+	 * Metoda vraća vrednost primarnog ključa tabele u bazi u kojoj se nalaze podaci o stavci kataloga
+	 * 
+	 * @return null koji predstavlja vrednost primarnog kljuca, jer je primarni ključ složen.
+	 */
 	@Override
 	public Long getVrednostPrimarnogKljuca() {
 		return null;
 	}
 
+	/**
+	 * Metoda konvertuje prosledjeni ResultSet u listu objekata klase StavkaKataloga 
+	 * 
+	 * @param rs ResultSet koji je potrebno konvertovati u listu stavki kataloga
+	 * 
+	 * @return Lista domenskih objekata koji su pročitani iz baze
+	 */
 	@Override
 	public List<OpstiDomenskiObjekat> konvertujRSUListu(ResultSet rs) {
 		List<OpstiDomenskiObjekat> stavke = new ArrayList<>();
@@ -106,11 +214,21 @@ public class StavkaKataloga implements OpstiDomenskiObjekat, Serializable{
         return stavke;
 	}
 
+	/**
+	 * Metoda vraća nazive atributa stavke kataloga i njihovih novih vrednosti koje ce biti ažurirane
+	 * 
+	 * @return String koji predstavlja kombinaciju parametara i njihovih novih vrednosti koje je potrebno ažurirati u bazi.
+	 */
 	@Override
 	public String getUpitAzuriranje() {
         return "katalogID = "+katalog.getKatalogID()+", stavkaKatalogaID = "+stavkaKatalogaID+", nazivStavkeKataloga = '"+nazivStavkeKataloga+"'";
 	}
 
+	/**
+	 * Metoda vraća uslov složenog primarnog ključa tabele u bazi koja predstavlja stavku kataloga
+	 * 
+	 * @return String koji predstavlja ime i vrednost složenog primarnog ključa 
+	 */
 	@Override
 	public String getSlozeniPrimarniKljuc() {
         return "WHERE katalogID="+katalog.getKatalogID()+" AND stavkaKataloga="+stavkaKatalogaID;
--- MvnRecepcijaBiblioteka/src/main/java/rs/ac/bg/fon/ai/nprog/MvnRecepcijaBiblioteka/domen/UslugaNajma.java
+++ MvnRecepcijaBiblioteka/src/main/java/rs/ac/bg/fon/ai/nprog/MvnRecepcijaBiblioteka/domen/UslugaNajma.java
@@ -9,17 +9,54 @@ import rs.ac.bg.fon.ai.nprog.MvnRecepcijaBiblioteka.domen.StavkaKataloga;
 import rs.ac.bg.fon.ai.nprog.MvnRecepcijaBiblioteka.domen.VrstaSobe;
 import rs.ac.bg.fon.ai.nprog.MvnRecepcijaBiblioteka.domen.OpstiDomenskiObjekat;
 
+/**
+ * Klasa predstavlja stavku kataloga sa svim svojim svojstvima. Implementira interfejse OpstiDomenskiObjekat i Serializable
+ *
+ * @Jelena Sreckovic
+ */
+
 public class UslugaNajma implements OpstiDomenskiObjekat, Serializable{
 
+	/**
+	 * Stavka kataloga koja predstavlja uslugu najma
+	 */
 	private StavkaKataloga stavkaKataloga;
+	
+	/**
+	 * Identifikator usluge najma
+	 */
     private Long uslugaNajmaID;
+    
+    /**
+     * Naziv usluge najma
+     */
     private String nazivUslugeNajma;
+    
+    /**
+     * Cena usluge najma
+     */
     private double cenaUsluge; 
+    
+    /**
+     * Vrsta sobe za koju je vezana usluga najma
+     */
     private VrstaSobe vrstaSobe; 
 
+    /**
+     * Neparametrizovani konstruktor. Inicijalizuje stavku kataloga
+     */
     public UslugaNajma() {
     }
 
+    /**
+     * Parametrizovani konstruktor. Inicijalizuje uslugu najma i postavlja vrednosti atributa pozivanjem set metoda.
+     * 
+     * @param stavkaKataloga stavka kataloga koju je potrebno postaviti
+     * @param uslugaNajmaID identifikator usluge koji je potrebno postaviti
+     * @param nazivUslugeNajma naziv usluge koji je potrebno postaviti
+     * @param cenaUsluge cena usluge koju je potrebno postaviti
+     * @param vrstaSobe vrsta sobe koju je potrebno postaviti
+     */
     public UslugaNajma(StavkaKataloga stavkaKataloga, Long uslugaNajmaID, String nazivUslugeNajma, double cenaUsluge, VrstaSobe vrstaSobe) {
         this.stavkaKataloga = stavkaKataloga;
         this.uslugaNajmaID = uslugaNajmaID;
@@ -28,80 +65,173 @@ public class UslugaNajma implements OpstiDomenskiObjekat, Serializable{
         this.vrstaSobe=vrstaSobe;
     }
 
+    /**
+     * Parametrizovani konstruktor. Inicijalizuje uslugu najma i postavlja vrednost atributa na unete vrednosti pozivanjem set metoda.
+     * 
+     * @param stavkaKataloga stavka kataloga koju je potrebno postaviti
+     * @param uslugaNajmaID identifikator koji je potrebno postaviti
+     */
     public UslugaNajma(StavkaKataloga stavkaKataloga, Long uslugaNajmaID) {
         this.stavkaKataloga = stavkaKataloga;
         this.uslugaNajmaID = uslugaNajmaID;
     }
     
-    public UslugaNajma(Long uslugaID) {
-        this.uslugaNajmaID=uslugaID;
+    /**
+     * Parametrizovani konstruktor. Inicijalizuje uslugu najma i postavlja vrednost identifikatora na unetu vrednost pozivanjem set metode.
+     * 
+     * @param uslugaNajmaID identifikator koji je potrebno postaviti
+     */
+    public UslugaNajma(Long uslugaNajmaID) {
+        this.uslugaNajmaID=uslugaNajmaID;
     }
 	
+    /**
+     * Metoda vraća stavku kataloga koja predstavlja uslugu najma
+     * 
+     * @return stavkaKataloga kao StavkaKataloga
+     */
 	public StavkaKataloga getStavkaKataloga() {
 		return stavkaKataloga;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost stavke kataloga koja predstavlja uslugu najma 
+	 * 
+	 * @param stavkaKataloga Stavka kataloga koju je potrebno postaviti
+	 */
 	public void setStavkaKataloga(StavkaKataloga stavkaKataloga) {
 		this.stavkaKataloga = stavkaKataloga;
 	}
 
+	/**
+     * Metoda vraća identifikator usluge najma
+     * 
+     * @return uslugaNajmaID kao Long
+     */
 	public Long getUslugaNajmaID() {
 		return uslugaNajmaID;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost identifikatora usluge najma
+	 * 
+	 * @param uslugaNajmaID identifikator usluge najma koji je potrebno postaviti
+	 */
 	public void setUslugaNajmaID(Long uslugaNajmaID) {
 		this.uslugaNajmaID = uslugaNajmaID;
 	}
 
+	/**
+     * Metoda vraća naziv usluge najma
+     * 
+     * @return nazivUslugeNajma kao String
+     */
 	public String getNazivUslugeNajma() {
 		return nazivUslugeNajma;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost naziva usluge najma
+	 * 
+	 * @param nazivUslugeNajma naziv usluge najma koji je potrebno postaviti
+	 */
 	public void setNazivUslugeNajma(String nazivUslugeNajma) {
 		this.nazivUslugeNajma = nazivUslugeNajma;
 	}
 
+	/**
+     * Metoda vraća cenu usluge najma
+     * 
+     * @return cenaUsluge kao double
+     */
 	public double getCenaUsluge() {
 		return cenaUsluge;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost cene usluge najma
+	 * 
+	 * @param cenaUsluge cena usluge najma koju je potrebno postaviti
+	 */
 	public void setCenaUsluge(double cenaUsluge) {
 		this.cenaUsluge = cenaUsluge;
 	}
 
+	/**
+	 * Metoda vraća vrstu sobe za koju je usluga najma vezana
+	 * 
+	 * @return vrstaSobe kao VrstaSobe
+	 */
 	public VrstaSobe getVrstaSobe() {
 		return vrstaSobe;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost vrste sobe za koju je usluga najma vezana
+	 * 
+	 * @param vrstaSobe Vrsta sobe koju je potrebno postaviti
+	 */
 	public void setVrstaSobe(VrstaSobe vrstaSobe) {
 		this.vrstaSobe = vrstaSobe;
 	}
 
+	/**
+   	 * Metoda vraća naziv tabele u bazi u kojoj se nalaze podaci o usluzi najma
+   	 * 
+   	 * @return String koji predstavlja naziv tabele.
+   	 */
 	@Override
 	public String getImeTabele() {
 		return "uslugaNajma";
 	}
 
+	/**
+	 * Metoda vraća vrednosti atributa objekta klase UslugaNajma
+	 * 
+	 * @return String koji predstavlja vrednosti atributa.
+	 */
 	@Override
 	public String getParametre() {
         return String.format("%s, %s, '%s', %s, %s",stavkaKataloga.getStavkaKatalogaID(),uslugaNajmaID,nazivUslugeNajma,cenaUsluge,vrstaSobe.getVrstaSobeID()); //vidi format za jak-slab
 	}
 
+	/**
+	 * Metoda vraća imena atributa objekta klase usluga najma nad kojim se poziva metoda
+	 * 
+	 * @return String koji predstavlja imena atributa, odvojeni su zapetom.
+	 */
 	@Override
 	public String getImenaParametara() {
         return "stavkaKatalogaID,uslugaNajmaID,nazivUslugeNajma,cenaUsluge,vrstaSobeID";
 	}
 
+	/**
+   	 * Metoda vraća ime primarnog kljuca tabele u bazi u kojoj se nalaze podaci o usluzi
+   	 * 
+   	 * @return null koji predstavlja ime primarnog ključa, jer je primarni kljuc slozen.
+   	 */
 	@Override
 	public String getImePrimarnogKljuca() {
 		return null;
 	}
 
+	/**
+	 * Metoda vraca vrednost primarnog kljuca tabele u kojoj se nalaze podaci o usluzi najma
+	 * 
+	 * @return null koji predstavlja vrednost primarnog kljuca, jer je primarni ključ složen.
+	 */
 	@Override
 	public Long getVrednostPrimarnogKljuca() {
 		return null;
 	}
 
+	/**
+	 * Metoda konvertuje prosledjeni ResultSet u listu objekata klase UslugaNajma 
+	 * 
+	 * @param rs ResultSet koji je potrebno konvertovati u listu usluga
+	 * 
+	 * @return Lista domenskih objekata koji su procitani iz baze
+	 */
 	@Override
 	public List<OpstiDomenskiObjekat> konvertujRSUListu(ResultSet rs) {
 		List<OpstiDomenskiObjekat> usluge = new ArrayList<>();
@@ -124,16 +254,27 @@ public class UslugaNajma implements OpstiDomenskiObjekat, Serializable{
         return usluge;
 	}
 
+	/**
+	 * Metoda vraća nazive atributa usluge najma i njihovih novih vrednosti koje ce biti ažurirane
+	 * 
+	 * @return String koji predstavlja kombinaciju parametara i njihovih novih vrednosti koje je potrebno ažurirati u bazi.
+	 */
 	@Override
 	public String getUpitAzuriranje() {
         return "stavkaKatalogaID = "+stavkaKataloga.getStavkaKatalogaID()+", uslugaNajmaID = "+uslugaNajmaID+", nazivUslugeNajma = '"+nazivUslugeNajma+"', cenaUsluge = "+cenaUsluge+", vrstaSobeID = "+vrstaSobe.getVrstaSobeID();
 	}
 
+	/**
+	 * Metoda vraća uslov složenog primarnog ključa tabele u bazi koja predstavlja uslugu najma
+	 * 
+	 * @return String koji predstavlja ime i vrednost složenog primarnog kljuca.
+	 */
 	@Override
 	public String getSlozeniPrimarniKljuc() {
         return "stavkaKatalogaID="+stavkaKataloga.getStavkaKatalogaID()+" AND uslugaNajmaID="+uslugaNajmaID+ " AND vrstaSobeID="+vrstaSobe.getVrstaSobeID();
 	}
 
+	
 	public String getComplexPrimaryKeyNames() {
         return "stavkaKatalogaID,uslugaNajmaID,vrstaSobeID";
     }
