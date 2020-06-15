8562ab7086c9978c2464be13be08c162aa66dfe3
Jelena-S <jsreckovic1@gmail.com>
2020-06-16 01:50:09 +0200

    Dodata dokumentacija za klase Soba i VrstaSobe

--- MvnRecepcijaBiblioteka/src/main/java/rs/ac/bg/fon/ai/nprog/MvnRecepcijaBiblioteka/domen/Soba.java
+++ MvnRecepcijaBiblioteka/src/main/java/rs/ac/bg/fon/ai/nprog/MvnRecepcijaBiblioteka/domen/Soba.java
@@ -8,75 +8,168 @@ import java.util.List;
 import rs.ac.bg.fon.ai.nprog.MvnRecepcijaBiblioteka.domen.VrstaSobe;
 import rs.ac.bg.fon.ai.nprog.MvnRecepcijaBiblioteka.domen.OpstiDomenskiObjekat;
 
+/**
+ * Klasa predstavlja sobu sa svim svojim svojstvima. Implementira interfejse OpstiDomenskiObjekat i Serializable
+ *
+ * @author Jelena Sreckovic
+ */
+
 public class Soba implements OpstiDomenskiObjekat, Serializable{
+	/**
+	 * Identifikator sobe
+	 */
 	private Long sobaID;
+	
+	/**
+	 * Status sobe
+	 */
     private boolean status;
+    
+    /**
+     * Vrsta sobe koja odgovara sobi
+     */
     private VrstaSobe vrstaSobe;
 
+    /**
+     * Neparametrizovani konstruktor. Inicijalizuje sobu.
+     */
     public Soba() {
     	
     }
     
+    /**
+     * Parametrizovani konstruktor. Inicijalizuje sobu i postavlja vrednosti pozivanjem set metoda.
+     * 
+     * @param sobaID identifikator sobe koji je potrebno postaviti
+     * @param status status sobe koji j epotrebno postaviti
+     * @param vrstaSobe vrsta sobe koju je potrebno postaviti
+     */
     public Soba(Long sobaID, boolean status, VrstaSobe vrstaSobe) {
         this.sobaID = sobaID;
         this.status = status;
         this.vrstaSobe = vrstaSobe;
     }
     
+    /**
+     * Parametrizovani konstruktor. Inicijalizuje sobu i postavlja identifikator  na unetu vrednost pozivanjem set metode.
+     * 
+     * @param sobaID identifikator sobe koji je potrebno postaviti
+     */
     public Soba(Long sobaID) {
         this.sobaID = sobaID;
     }
 	
+    /**
+     * Metoda vraća identifikator sobe
+     * 
+     * @return sobaID kao Long
+     */
 	public Long getSobaID() {
 		return sobaID;
 	}
 
+	/**
+     * Metoda postavlja identifikator sobe
+     * 
+     * @param sobaID nova vrednost identifikatora sobe koju je potrebno postaviti
+     */
 	public void setSobaID(Long sobaID) {
 		this.sobaID = sobaID;
 	}
 
+	/**
+     * Metoda vraća status sobe
+     * 
+     * @return status kao boolean
+     */
 	public boolean isStatus() {
 		return status;
 	}
 
+	/**
+     * Metoda postavlja status sobe
+     * 
+     * @param status nova vrednost statusa koju je potrebno postaviti
+     */
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
 
+	/**
+     * Metoda vraća vrstu sobe
+     * 
+     * @return vrstaSobe kao objekat klase VrstaSobe
+     */
 	public VrstaSobe getVrstaSobe() {
 		return vrstaSobe;
 	}
 
+	/**
+     * Metoda postavlja vrstu sobe 
+     * 
+     * @param vrstaSobe nova vrsta sobe koju je potrebno postaviti
+     */
 	public void setVrstaSobe(VrstaSobe vrstaSobe) {
 		this.vrstaSobe = vrstaSobe;
 	}
 	
-
+	/**
+   	 * Metoda vraća naziv tabele u bazi u kojoj se nalaze podaci o sobi.
+   	 * 
+   	 * @return String koji predstavlja naziv tabele.
+   	 */
 	@Override
 	public String getImeTabele() {
 		return "soba";
 	}
 
+	/**
+	 * Metoda vraća vrednosti atributa objekta klase Soba
+	 * 
+	 * @return String koji predstavlja vrednosti atributa.
+	 */
 	@Override
 	public String getParametre() {
         return String.format("%s, '%s', %s", sobaID,status,vrstaSobe.getVrstaSobeID());
 	}
 
+	/**
+	 * Metoda vraća imena atributa objekta klase Soba nad kojim se poziva metoda
+	 * 
+	 * @return String koji predstavlja imena atributa, odvojeni su zapetom.
+	 */
 	@Override
 	public String getImenaParametara() {
         return "sobaID,status,vrstaSobeID";
 	}
 
+	/**
+   	 * Metoda vraća ime primarnog ključa tabele u bazi u kojoj se nalaze podaci o sobi
+   	 * 
+   	 * @return String koji predstavlja ime primarnog ključa.
+   	 */
 	@Override
 	public String getImePrimarnogKljuca() {
 		return "sobaID";
 	}
 
+	/**
+	 * Metoda vraća vrednost primarnog ključa tabele u bazi u kojoj se nalaze podaci o sobi
+	 * 
+	 * @return sobaID kao Long koji predstavlja vrednost primarnog ključa.
+	 */
 	@Override
 	public Long getVrednostPrimarnogKljuca() {
 		return sobaID;
 	}
 
+	/**
+	 * Metoda konvertuje ResultSet u listu objekata klase Soba 
+	 * 
+	 * @param rs ResultSet koji je potrebno konvertovati u listu soba
+	 * 
+	 * @return Lista domenskih objekata koji su procitani iz baze
+	 */
 	@Override
 	public List<OpstiDomenskiObjekat> konvertujRSUListu(ResultSet rs) {
 		List<OpstiDomenskiObjekat> rooms = new ArrayList<>();
@@ -97,11 +190,21 @@ public class Soba implements OpstiDomenskiObjekat, Serializable{
         return rooms;   
 	}
 
+	/**
+	 * Metoda vraca nazive atributa sobe i njihovih novih vrednosti koje ce biti ažurirane
+	 * 
+	 * @return String koji predstavlja kombinaciju parametara i njihovih novih vrednosti koje je potrebno ažurirati u bazi.
+	 */
 	@Override
 	public String getUpitAzuriranje() {
         return "sobaID = "+sobaID+", status = "+status+", vrstaSobeID = "+vrstaSobe.getVrstaSobeID();
 	}
 
+	/**
+	 * Metoda vraća uslov složenog primarnog ključa tabele u bazi koja predstavlja sobu.
+	 * 
+	 * @return null koji predstavlja ime i vrednost složenog primarnog ključa, jer primarni ključ nije složen.
+	 */
 	@Override
 	public String getSlozeniPrimarniKljuc() {
 		return null;
--- MvnRecepcijaBiblioteka/src/main/java/rs/ac/bg/fon/ai/nprog/MvnRecepcijaBiblioteka/domen/VrstaSobe.java
+++ MvnRecepcijaBiblioteka/src/main/java/rs/ac/bg/fon/ai/nprog/MvnRecepcijaBiblioteka/domen/VrstaSobe.java
@@ -8,98 +8,226 @@ import java.util.List;
 import rs.ac.bg.fon.ai.nprog.MvnRecepcijaBiblioteka.domen.UslugaNajma;
 import rs.ac.bg.fon.ai.nprog.MvnRecepcijaBiblioteka.domen.OpstiDomenskiObjekat;
 
+/**
+ * Klasa predstavlja vrstu sobe sa svim svojim svojstvima. Implementira interfejse OpstiDomenskiObjekat i Serializable
+ *
+ * @Jelena Sreckovic
+ */
+
 public class VrstaSobe implements OpstiDomenskiObjekat, Serializable{
+	/**
+	 * Identifikator vrste sobe
+	 */
 	private Long vrstaSobeID;
+	
+	/**
+	 * Naziv vrste sobe
+	 */
     private String nazivVrsteSobe;
+    
+    /**
+     * Broj kreveta koji odgovara vrsti sobe
+     */
     private int brojKreveta;
+    
+    /**
+     * Opis vrste sobe
+     */
     private String opis; 
+    
+    /**
+     * Lista usluga koje su vezane za vrstu sobe
+     */
     private List<UslugaNajma> usluge; 
 
+    /**
+     * Neparametrizovani konstruktor. Inicijalizuje vrstu sobe i listu usluga.
+     */
     public VrstaSobe() {
     	usluge=new ArrayList<>();
     }
 
-    public VrstaSobe( Long vrstaSobeID, String nazivVrsteSobe, int brojKreveta, String opis) {//, List<UslugaNajma> usluge
+    /**
+     * Parametrizovani konstruktor. Inicijalizuje vrstu sobe i postavlja vrednosti atributa pozivanjem set metoda.
+     * 
+     * @param vrstaSobeID identifikator vrste sobe koje je potrebno postaviti
+     * @param nazivVrsteSobe naziv vrste sobe koji je potrebno postaviti
+     * @param brojKreveta broj kreveta koji je potrebno postaviti
+     * @param opis opis vrste sobe koji je potrebno postaviti
+     */
+    public VrstaSobe( Long vrstaSobeID, String nazivVrsteSobe, int brojKreveta, String opis) {
         this.vrstaSobeID = vrstaSobeID;
         this.nazivVrsteSobe = nazivVrsteSobe;
         this.brojKreveta = brojKreveta;
         this.opis = opis;
     }
 
+    /**
+     * Parametrizovani konstruktor. Inicijalizuje vrstu sobe i postavlja vrednost identifikatora na unetu vrednost pozivanjem set metode.
+     * 
+     * @param vrstaSobeID identifikator koji je potrebno postaviti
+     */
     public VrstaSobe(Long vrstaSobeID) {
         this.vrstaSobeID = vrstaSobeID;
     }
 	
+    /**
+     * Metoda vraća identifikator vrste sobe
+     * 
+     * @return vrstaSobeID
+     */
 	public Long getVrstaSobeID() {
 		return vrstaSobeID;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost identifikatora vrste sobe
+	 * 
+	 * @param vrstaSobeID identifikator vrste sobe koji je potrebno postaviti
+	 */
 	public void setVrstaSobeID(Long vrstaSobeID) {
 		this.vrstaSobeID = vrstaSobeID;
 	}
 
+	/**
+	 * Metoda vraća naziv vrste sobe
+	 * 
+	 * @return nazivVrsteSobe kao String
+	 */
 	public String getNazivVrsteSobe() {
 		return nazivVrsteSobe;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost naziva vrste sobe
+	 * 
+	 * @param nazivVrsteSobe naziv vrste sobe koji je potrebno postaviti
+	 */
 	public void setNazivVrsteSobe(String nazivVrsteSobe) {
 		this.nazivVrsteSobe = nazivVrsteSobe;
 	}
 
+	/**
+	 * Metoda vraća broj kreveta koji odgovara vrsti sobe
+	 * 
+	 * @return brojKreveta kao int
+	 */
 	public int getBrojKreveta() {
 		return brojKreveta;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost broja kreveta koji odgovara vrsti sobe
+	 * 
+	 * @param brojKreveta broj kreveta koji je potrebno postaviti
+	 */
 	public void setBrojKreveta(int brojKreveta) {
 		this.brojKreveta = brojKreveta;
 	}
 
+	/**
+	 * Metoda vraća opis koji odgovara vrsti sobe
+	 * 
+	 * @return opis kao String
+	 */
 	public String getOpis() {
 		return opis;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost opisa koji odgovara vrsti sobe
+	 * 
+	 * @param opis Opis koji je potrebno postaviti
+	 */
 	public void setOpis(String opis) {
 		this.opis = opis;
 	}
 
+	/**
+	 * Metoda vraća listu usluga koje odgovara vrsti sobe
+	 * 
+	 * @return usluge kao List<UslugaNajma>
+	 */
 	public List<UslugaNajma> getUsluge() {
 		return usluge;
 	}
 
+	/**
+	 * Metoda postavlja novu vrednost usluga koje odgovaraju vrsti sobe
+	 * 
+	 * @param usluge usluge koje je potrebno postaviti
+	 */
 	public void setUsluge(List<UslugaNajma> usluge) {
 		this.usluge = usluge;
 	}
 	
+	/**
+	 * Metoda vraća String koji sadrži naziv vrste sobe
+     * 
+     * @return String koji sadrži naziv vrste sobe
+	 */
 	@Override
     public String toString() {
         return nazivVrsteSobe;
     }
 
+	/**
+   	 * Metoda vraća naziv tabele u bazi u kojoj se nalaze podaci o vrsti sobe
+   	 * 
+   	 * @return String koji predstavlja naziv tabele.
+   	 */
 	@Override
 	public String getImeTabele() {
 		return "vrstaSobe";
 	}
 
+	/**
+	 * Metoda vraća vrednosti atributa objekta klase VrstaSobe
+	 * 
+	 * @return String koji predstavlja vrednosti atributa.
+	 */
 	@Override
 	public String getParametre() {
         return String.format("%s, '%s', %s, '%s'",vrstaSobeID,nazivVrsteSobe,brojKreveta,opis);
 	}
 
+	/**
+	 * Metoda vraća imena atributa objekta klase VrstaSobe nad kojim se poziva metoda
+	 * 
+	 * @return String koji predstavlja imena atributa, odvojeni su zapetom.
+	 */
 	@Override
 	public String getImenaParametara() {
         return "vrstaSobeID,nazivVrsteSobe,brojKreveta,opis"; 
 	}
 
+	/**
+   	 * Metoda vraća ime primarnog kljuca tabele u bazi u kojoj se nalaze podaci o vrsti sobe
+   	 * 
+   	 * @return String koji predstavlja ime primarnog ključa.
+   	 */
 	@Override
 	public String getImePrimarnogKljuca() {
 		return "vrstaSobeID";
 	}
 
+	/**
+	 * Metoda vraća vrednost primarnog ključa tabele u bazi u kojoj se nalaze podaci o vrsti sobe
+	 * 
+	 * @return vrstaSobeID kao long 
+	 */
 	@Override
 	public Long getVrednostPrimarnogKljuca() {
 		return vrstaSobeID;
 	}
 
+	/**
+	 * Metoda konvertuje prosledjeni ResultSet u listu objekata klase VrstaSobe 
+	 * 
+	 * @param rs ResultSet koji je potrebno konvertovati u listu vrsta 
+	 * 
+	 * @return Lista domenskih objekata koji su procitani iz baze
+	 */
 	@Override
 	public List<OpstiDomenskiObjekat> konvertujRSUListu(ResultSet rs) {
 		List<OpstiDomenskiObjekat> vrste = new ArrayList<>();
@@ -123,11 +251,21 @@ public class VrstaSobe implements OpstiDomenskiObjekat, Serializable{
         return vrste;
 	}
 
+	/**
+	 * Metoda vraća nazive atributa vrste sobe i njihovih novih vrednosti koje će biti ažurirane
+	 * 
+	 * @return String koji predstavlja kombinaciju parametara i njihovih novih vrednosti koje je potrebno ažurirati u bazi.
+	 */
 	@Override
 	public String getUpitAzuriranje() {
         return "vrstaSobeID = "+vrstaSobeID+", nazivVrsteSobe = '"+nazivVrsteSobe+"', brojKreveta = "+brojKreveta+", opis = '"+opis+"'"; 
 	}
 
+	/**
+	 * Metoda vraća uslov složenog primarnog ključa tabele u bazi koja predstavlja vrstu sobe
+	 * 
+	 * @return null koji predstavlja ime i vrednost slozenog primarnog kljuca, jer primarni ključ nije složen.
+	 */
 	@Override
 	public String getSlozeniPrimarniKljuc() {
 		return null;
