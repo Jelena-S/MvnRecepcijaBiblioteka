358a2875b26970768fe0ff81e1e8a5ef2743e33f
Jelena-S <jsreckovic1@gmail.com>
2020-07-05 02:17:20 +0200

    Dodata klasa Forme koja sadrzi konstante koje predstavljaju pojedine forme.

---
+++ MvnRecepcijaBiblioteka/src/main/java/rs/ac/bg/fon/ai/nprog/MvnRecepcijaBiblioteka/konstante/Forme.java
@@ -0,0 +1,8 @@
+package rs.ac.bg.fon.ai.nprog.MvnRecepcijaBiblioteka.konstante;
+
+import java.io.Serializable;
+
+public class Forme implements Serializable{
+	public static final int SEARCH_ZAKUP_FORMA = 1;
+    public static final int SEARCH_ROOM_FORMA = 2;
+}
